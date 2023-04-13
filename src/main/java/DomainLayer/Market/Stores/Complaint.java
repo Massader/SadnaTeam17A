d9f9c0bfc6d88de4974d33e68910154be9bf8cfa
Roei Birnfeld <roeibir@post.bgu.ac.il>
2023-04-13 20:06:06 +0300

    (#46 Implement Messages Functionalities)

--- src/main/java/DomainLayer/Market/Stores/Complaint.java
+++ src/main/java/DomainLayer/Market/Stores/Complaint.java
@@ -1,4 +1,37 @@
 package DomainLayer.Market.Stores;
 
+import java.util.UUID;
+
 public class Complaint {
+    private boolean open;
+    private UUID userId;
+    private UUID purchaseId;
+    private UUID messageId;
+
+    public Complaint(UUID userId, UUID purchaseId, UUID messageId){
+        this.userId = userId;
+        this.purchaseId = purchaseId;
+        this.messageId = messageId;
+    }
+
+    public boolean isOpen(){
+        return open;
+    }
+
+    public void closeComplaint(){
+        this.open = false;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public UUID getPurchaseId() {
+        return purchaseId;
+    }
+
+    public UUID getMessageId() {
+        return messageId;
+    }
+
 }
