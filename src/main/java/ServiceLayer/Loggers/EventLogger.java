38a9af69fe8ef4acbbe39f4dcff6a28a2e61ade1
Nitzan Mass-Ader <nitzanmassader@Nitzans-MacBook-Pro.local>
2023-04-15 11:03:21 +0300

    (#28 - Logger) Added filehandler to logger classes

--- src/main/java/ServiceLayer/Loggers/EventLogger.java
+++ src/main/java/ServiceLayer/Loggers/EventLogger.java
@@ -2,6 +2,8 @@ package ServiceLayer.Loggers;
 
 import ServiceLayer.Service;
 
+import java.nio.file.Path;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import static java.util.logging.Logger.getLogger;
@@ -10,9 +12,12 @@ public class EventLogger {
     private static final Logger LOG = getLogger("EventLogger");
     private static EventLogger instance = null;
     private static Object instanceLock = new Object();
+    private static FileHandler fileHandler;
 
     private EventLogger(){
-
+        try {
+            fileHandler = new FileHandler(System.getProperty("user.dir") + "/EventLog.log");
+        } catch (Exception ignored) {}
     }
     public static EventLogger getInstance() {
         synchronized (instanceLock) {
