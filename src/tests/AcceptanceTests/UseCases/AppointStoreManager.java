242d3961295a65c65289b9a749dfb8d6d1a27d9e
Lior Levy <levyaz@post.bgu.ac.il>
2023-04-16 12:34:54 +0300

    accTest Appoint Store Manager

--- src/tests/AcceptanceTests/UseCases/AppointStoreManager.java
+++ src/tests/AcceptanceTests/UseCases/AppointStoreManager.java
@@ -1,13 +1,58 @@
 package AcceptanceTests.UseCases;
 import AcceptanceTests.*;
+import ServiceLayer.ServiceObjects.ServiceStore;
+import org.junit.*;
+
+import java.util.UUID;
 
 public class AppointStoreManager extends ProjectTest {
 
-    public AppointStoreManager(Bridge real) {
-        super(real);
+    UUID founder;
+    UUID client;
+    UUID storeManager;
+    UUID client2;
+    ServiceStore store;
+    UUID storeId;
+
+    @BeforeClass
+    public void setUp() {
+        bridge.setReal();
+        bridge.register("founder", "pass");
+        client = bridge.enterSystem();
+        founder = bridge.login(client, "founder", "pass");
+        store = bridge.openStore(founder, "test", "test");
+        storeId = store.getStoreId();
+        bridge.register("lior", "pass");
+        client2 = bridge.enterSystem();
+        storeManager = bridge.login(client2, "toreManager", "pass");
+
     }
 
-    public boolean appointStoreManager() {
-        return bridge.appointStoreManager();
+    @Before
+    public void beforeEach()  {
+        client = bridge.enterSystem();
     }
-}
+
+    @After
+    public void tearDown() {
+        bridge.exitSystem(client);
+    }
+
+    @AfterClass
+    public void afterClass() {
+        bridge.closeStore(founder, storeId);
+        bridge.logout(founder);
+    }
+
+    @Test
+    public void AppointStoreManagerSuccess() {
+    Boolean AppointStoreManager = bridge.appointStoreManager(founder,storeManager,storeId);
+    Assert.assertTrue(AppointStoreManager);
+    }
+    @Test
+    public void AppointStoreManagerFail() {
+        Boolean AppointStoreManager = bridge.appointStoreManager(storeManager,founder,storeId);
+        Assert.assertFalse(AppointStoreManager);
+    }
+    }
+
