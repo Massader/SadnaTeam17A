4676da40e5a343ed347626af3c08e957b6521cf8
Lior <wlior@post.bgu.ac.il>
2023-05-06 19:16:01 +0300

    accTest: AppointStoreManager AppointStoreOwner CloseClient EnterSystem ExitSystem OpenStore

--- src/tests/AcceptanceTests/UseCases/EnterSystem.java
+++ src/tests/AcceptanceTests/UseCases/EnterSystem.java
@@ -2,6 +2,8 @@ package AcceptanceTests.UseCases;
 import AcceptanceTests.*;
 
 import java.util.UUID;
+
+import ServiceLayer.Response;
 import org.junit.*;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -12,30 +14,53 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class EnterSystem extends ProjectTest {
 
-    UUID clientCredentials;
-
     @BeforeAll
     public void setUp() {
-        bridge.setReal();
-    }
-
-    @BeforeEach
-    public void beforeEach()  {
-    }
-
-    @AfterEach
-    public void tearDown() {
-        bridge.closeClient(clientCredentials);
+        bridge.resetService();
     }
 
     @Test
     //checks if a client can enter the system by creating a new client credentials and ensuring that the client credentials is not null.
     public void enterSystemSuccess() {
-        int clients0 = bridge.numOfClients().getValue();
-        clientCredentials = bridge.createClient().getValue();
-        int clients1 = bridge.numOfClients().getValue();
+        Response<Integer> clients0 = bridge.numOfClients();
+        Response<UUID> client = bridge.createClient();
+        Response<Integer> clients1 = bridge.numOfClients();
 
-        Assert.assertNotNull(clientCredentials);
-        Assert.assertEquals(clients0 + 1, clients1);
+        Assert.assertFalse(clients0.isError());
+        Assert.assertFalse(client.isError());
+        Assert.assertFalse(clients1.isError());
+
+        Assert.assertNotNull(client.getValue());
+        Assert.assertEquals(1, clients1.getValue() - clients0.getValue());
+    }
+
+    @Test
+    //check if the system can accommodate 1000 clients concurrently.
+    public void enterSystemConcurrently(){
+        Response<Integer> clients0 = bridge.numOfClients();
+
+        Response<UUID>[] responses = new Response[1000];
+        Thread[] threads = new Thread[1000];
+        try {
+            for (int i = 0; i < 1000; i++) {
+                final int index = i;
+                threads[i] = new Thread(() -> responses[index] = bridge.createClient());
+                threads[i].start();
+            }
+            for (Thread t : threads) {
+                t.join();
+            }
+        }
+        catch (Exception ignore) {}
+
+        Response<Integer> clients1 = bridge.numOfClients();
+
+        Assert.assertFalse(clients0.isError());
+        Assert.assertFalse(clients1.isError());
+        for (Response<UUID> r : responses) {
+            Assert.assertFalse(r.isError());
+            Assert.assertNotNull(r.getValue());
+        }
+        Assert.assertEquals(1000, clients1.getValue() - clients0.getValue());
     }
 }
--- src/tests/AcceptanceTests/UseCases/ExitSystem.java
+++ src/tests/AcceptanceTests/UseCases/ExitSystem.java
@@ -2,6 +2,8 @@ package AcceptanceTests.UseCases;
 import AcceptanceTests.*;
 
 import java.util.UUID;
+
+import ServiceLayer.Response;
 import org.junit.*;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -12,28 +14,57 @@ import org.junit.jupiter.api.TestInstance;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ExitSystem extends ProjectTest {
 
-    UUID clientCredentials;
-
-    @Before
-    public void beforeEach()  {
-        bridge.setReal();
-        clientCredentials = bridge.createClient().getValue();
-    }
-
-    @After
-    public void tearDown() {
-        bridge.closeClient(clientCredentials);
+    @BeforeAll
+    public void setUp() {
+        bridge.resetService();
     }
 
     @Test
-    //Tests that a client can successfully exit the system. It creates a client and then attempts to close it, asserting that the operation is successful.
+    //checks if a client can exit the system.
     public void exitSystemSuccess() {
-        int clients0 = bridge.numOfClients().getValue();
-        Boolean success = bridge.closeClient(clientCredentials).getValue();
-        int clients1 = bridge.numOfClients().getValue();
+        UUID client = bridge.createClient().getValue();
+        Response<Integer> clients0 = bridge.numOfClients();
+        Response<Boolean> exit = bridge.closeClient(client);
+        Response<Integer> clients1 = bridge.numOfClients();
 
-        Assert.assertNotNull(success);
-        Assert.assertTrue(success);
-        Assert.assertEquals(clients0 - 1, clients1);
+        Assert.assertFalse(clients0.isError());
+        Assert.assertFalse(exit.isError());
+        Assert.assertFalse(clients1.isError());
+
+        Assert.assertTrue(exit.getValue());
+        Assert.assertEquals(1, clients0.getValue() - clients1.getValue());
+    }
+
+    @Test
+    //check if 1000 clients can exit the system concurrently.
+    public void enterSystemConcurrently(){
+        UUID[] clients = new UUID[1000];
+        for (int i = 0; i < 1000; i++) {
+            clients[i] = bridge.createClient().getValue();
+        }
+
+        Response<Integer> clients0 = bridge.numOfClients();
+        Response<Boolean>[] responses = new Response[1000];
+        Thread[] threads = new Thread[1000];
+        try {
+            for (int i = 0; i < 1000; i++) {
+                final int index = i;
+                threads[i] = new Thread(() -> responses[index] = bridge.closeClient(clients[index]));
+                threads[i].start();
+            }
+            for (Thread t : threads) {
+                t.join();
+            }
+        }
+        catch (Exception ignore) {}
+        Response<Integer> clients1 = bridge.numOfClients();
+
+        Assert.assertFalse(clients0.isError());
+        Assert.assertFalse(clients1.isError());
+        for (Response<Boolean> r : responses) {
+            Assert.assertFalse(r.isError());
+            Assert.assertTrue(r.getValue());
+        }
+        Assert.assertEquals(1000, clients0.getValue() - clients1.getValue());
     }
 }
