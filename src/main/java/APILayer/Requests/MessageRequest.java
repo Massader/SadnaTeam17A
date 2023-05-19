15328ff8c2b44d31b5e17e10431c5aebe9466938
Nitzan <massader@post.bgu.ac.il>
2023-05-19 17:21:29 +0300

    Fixed message request object

--- src/main/java/APILayer/Requests/MessageRequest.java
+++ src/main/java/APILayer/Requests/MessageRequest.java
@@ -3,27 +3,15 @@ package APILayer.Requests;
 import java.util.UUID;
 
 public class MessageRequest extends Request{
-    private java.util.UUID clientCredentials;
     private UUID sender;
     private UUID recipient;
     private String body;
 
-    public MessageRequest(UUID clientCredentials, UUID clientCredentials1, UUID sender, UUID recipient, String body) {
+    public MessageRequest(UUID clientCredentials, UUID sender, UUID recipient, String body) {
         super(clientCredentials);
-        this.clientCredentials = clientCredentials1;
         this.sender = sender;
         this.recipient = recipient;
         this.body = body;
-    }
-
-    @Override
-    public UUID getClientCredentials() {
-        return clientCredentials;
-    }
-
-    @Override
-    public void setClientCredentials(UUID clientCredentials) {
-        this.clientCredentials = clientCredentials;
     }
 
     public UUID getSender() {
