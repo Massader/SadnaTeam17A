80539e00d61bd8c88a22b04221c9dc42d6681fa1
Lior Levy <levyaz@post.bgu.ac.il>
2023-05-28 14:27:20 +0300

    Adding an explanation of the change

--- src/main/java/DomainLayer/Supply/SupplyBridge.java
+++ src/main/java/DomainLayer/Supply/SupplyBridge.java
@@ -5,5 +5,8 @@ import DomainLayer.Market.Users.ShoppingCart;
 public interface SupplyBridge {
     public void setReal();
     Boolean validateOrder(String address);
-    Integer sendOrder();
+    Integer sendOrder();//supply(name , address, city, country, zip)
+    //int cancel_supply(UUID transaction_id)// - the id of the transaction id of the transaction to be canceled.)
+    //SERVER= "https://php-server-try.000webhostapp.com/"
+    //In order to specify which service to request from
 }
